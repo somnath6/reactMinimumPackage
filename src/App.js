75d622ff6995066746942280c49608e885a5d4fc
somnath6 <somnathmaity311@gmail.com>
2024-02-13 22:29:50 +0530

    add more feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import data from './data';
+import Tours from './components/Tour';
+import Card  from "./components/Card";
 
 const App = () => {
-  return <div>App</div>;
+  const[tours, setTour] = useState(data);
+  function removeTour(id){
+    const newTour = tours.filter(tour => tour.id !== id);
+    setTour(newTour);
+  }
+  if(tours.length === 0){
+    return(
+      <div className="refresh">
+          <h2>No Tours left</h2>
+          <button className="btn-white" onClick={() => setTour(data)}> 
+                Refresh
+          </button>
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      <Tours tours={tours} removeTour = {removeTour}></Tours>
+    </div>
+  );
 };
 
 export default App;
